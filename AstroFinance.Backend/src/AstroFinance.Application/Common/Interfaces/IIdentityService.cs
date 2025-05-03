bc736edb4beb7340ffedfa71adc7336e9f084d9f
notcool100 <anjaljoshi6@gmail.com>
2025-05-03 19:08:52 +0545

    test2

--- AstroFinance.Backend/src/AstroFinance.Application/Common/Interfaces/IIdentityService.cs
+++ AstroFinance.Backend/src/AstroFinance.Application/Common/Interfaces/IIdentityService.cs
@@ -1,5 +1,8 @@
 using AstroFinance.Application.Common.Models;
+using AstroFinance.Application.Users.Queries.GetUsersList;
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AstroFinance.Application.Common.Interfaces
@@ -12,5 +15,11 @@ namespace AstroFinance.Application.Common.Interfaces
         Task<(Result Result, string UserId)> CreateUserAsync(string userName, string password);
         Task<Result> DeleteUserAsync(string userId);
         Task<AuthenticationResult> AuthenticateAsync(string email, string password);
+        
+        // User management methods
+        Task<(List<UserDto> Users, int TotalCount)> GetUsersAsync(string? searchTerm, int pageNumber, int pageSize, CancellationToken cancellationToken);
+        Task<UserDto?> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken);
+        Task<Result> UpdateUserAsync(Guid userId, string email, string firstName, string lastName, List<string> roles, bool isActive, CancellationToken cancellationToken);
+        Task<Result> DeleteUserAsync(Guid userId, CancellationToken cancellationToken);
     }
 }
